290674f4d2d0b5034fe3abf250ad3d816eeb7908
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2023-08-07 22:57:56 +0530

    Project initialized

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-blue-700 text-7xl'>Hello world</h1>
+    </>
+  )
+}
+
+export default App
