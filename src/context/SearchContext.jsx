755a907e9a306fdc5a8a2e51495e96f960a044bf
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2023-08-22 00:31:10 +0530

    Search Context API created

---
+++ src/context/SearchContext.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+
+const SearchContext = createContext()
+
+export const MarketAPIProvider =({children})=>{
+    const [sdata, setSdata] = useState([])
+    const [search, setsearch] = useState('bitcoin')
+
+    const url = `https://api.coingecko.com/api/v3/search?query=${search}`
+
+
+    const getSearchData =()=>{
+        axios.get(url).then((response)=>{
+            setSdata(response.data)
+            console.log(response.data)
+
+        })
+    }
+
+    useEffect(()=>{
+        getSearchData()
+    },[url])
+
+
+    return(
+        <SearchContext.Provider value={{sdata}}>{children}</SearchContext.Provider>
+    )
+
+}
+
+export const useSearchContext =()=>{
+    return useContext(SearchContext)
+}
