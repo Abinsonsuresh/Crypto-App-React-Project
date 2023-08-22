0e63eba0d47300f24ae14c21fd30abd835c53f7b
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2023-08-23 01:16:02 +0530

    Footer component finished

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,11 +12,11 @@ import ThemeToggleBtn from './ThemeToggleBtn'
 const Footer = () => {
   return (
     <>
-    <div className='rounded-div mt-8 pt-8 text-primary flex flex-col md:flex-row gap-9 justify-between'>
-      <div className=' flex justify-center'>       
+    <div className='rounded-div mt-8 pt-8 text-primary flex flex-col md:flex-row gap-9 justify-start'>
+      <div className='flex justify-center'>       
               <div className='text-left'>
           <div className='w-full flex justify-start'>
-            <div className='w-full md:w-[300px] py-4 relative'>
+            <div className='w-full md:w-[300px] py-4 relative ml-5'>
               <div className='flex justify-center md:justify-start py-4 md:py-0 md:pb-4 mt-[-1rem]'>
                 <ThemeToggleBtn />
               </div>
@@ -30,7 +30,7 @@ const Footer = () => {
               <div className='flex py-4 justify-between text-accent'>
               <a href="https://www.linkedin.com/in/abinsonsuresh/" target="_blank"><FaLinkedin size={25}/></a>
               <a href="https://www.instagram.com/abinsonsuresh/" target="_blank">  <AiOutlineInstagram size={25} /></a>
-              <a href="https://twitter.com/abinsonsuresh" target="_blank">  <FaTwitter size={25} /></a>
+              <a href="https://twitter.com/abinsonsuresh/" target="_blank">  <FaTwitter size={25} /></a>
                 <a href="https://github.com/Abinsonsuresh/" target="_blank"> <FaGithub size={25}/></a>
                
                
@@ -40,30 +40,7 @@ const Footer = () => {
         </div>
 
     </div>
-    <div className='flex justify-center md:justify-end '>
-    <div className='grid md:grid-cols-2'>
-        <div className='flex justify-between w-full md:max-w-[300px] uppercase'>
-          <div>
-            <h2 className='font-bold'>Support</h2>
-            <ul>
-              <li className='text-sm py-2'>Help Center</li>
-              <li className='text-sm py-2'>Contact Us</li>
-              <li className='text-sm py-2'>API Status</li>
-              <li className='text-sm py-2'>Documentation</li>
-            </ul>
-          </div>
-          <div className='ml-10'>
-            <h2 className='font-bold'>Info</h2>
-            <ul>
-              <li className='text-sm py-2'>About Us</li>
-              <li className='text-sm py-2'>Careers</li>
-              <li className='text-sm py-2'>Invest</li>
-              <li className='text-sm py-2'>Legal</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
+
     </div>
       <p className='text-center py-4 '>Copyright © 2023 CryptoTrix - Created by Abinson Suresh</p>
       <div className='flex items-center justify-center space-x-2'>
@@ -77,28 +54,3 @@ const Footer = () => {
 
 export default Footer;
 
-
-
-
-      {/* <div className='grid md:grid-cols-2'>
-        <div className='flex justify-between w-full md:max-w-[300px] uppercase'>
-          <div>
-            <h2 className='font-bold'>Support</h2>
-            <ul>
-              <li className='text-sm py-2'>Help Center</li>
-              <li className='text-sm py-2'>Contact Us</li>
-              <li className='text-sm py-2'>API Status</li>
-              <li className='text-sm py-2'>Documentation</li>
-            </ul>
-          </div>
-          <div className='ml-10'>
-            <h2 className='font-bold'>Info</h2>
-            <ul>
-              <li className='text-sm py-2'>About Us</li>
-              <li className='text-sm py-2'>Careers</li>
-              <li className='text-sm py-2'>Invest</li>
-              <li className='text-sm py-2'>Legal</li>
-            </ul>
-          </div>
-        </div>
-      </div> */}
